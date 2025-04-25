e2a7d02ed20df1c4f589285670703fcd6a265658
leozzzzzzzzz <125218730+leozzzzzzzzz@users.noreply.github.com>
2025-04-25 18:08:41 -0300

    criando App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core/dist/cjs/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import Menu from "./components/Menu";
+import Home from "./components/screens/home";
+import About from "./components/screens/about";
+
+import Local from "./components/screens/local/Local";
+import Passagem from "./components/screens/passagem/Passagem";
+import Tipo from "./components/screens/tipo/Tipo";
+import Veiculo from "./components/screens/veiculo/Veiculo";
+
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Menu />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "sobre",
+        element: <About />,
+      },
+      {
+        path: "local",
+        element: <Local />,
+      },
+      {
+        path: "passagem",
+        element: <Passagem />,
+      },
+      {
+        path: "tipo",
+        element: <Tipo />,
+      },
+      {
+        path: "veiculo",
+        element: <Veiculo />,
+      }
+    ]
+  }
+])
+
+function App() { 
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App; 
