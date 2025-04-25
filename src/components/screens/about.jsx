2c170b3592622d9641326e85ff75a02455dba287
leozzzzzzzzz <125218730+leozzzzzzzzz@users.noreply.github.com>
2025-04-25 18:07:21 -0300

    feat: componentes basicos

---
+++ src/components/screens/about.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <>
+            <h1>
+                Sobre o sistema
+            </h1>
+            <h2>Trabalho da disciplina "Programação Web" do curso de Ciência da Computação</h2>
+        </>
+    )
+}
+
+export default About
