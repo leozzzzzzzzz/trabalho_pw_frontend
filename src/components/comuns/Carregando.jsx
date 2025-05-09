7f540505ec0691163e7242abae215d4c8c9e3d79
leozzzzzzzzz <125218730+leozzzzzzzzz@users.noreply.github.com>
2025-05-08 22:01:38 -0300

    fix: arrumando spinner

--- src/components/comuns/Carregando.jsx
+++ src/components/comuns/Carregando.jsx
@@ -7,7 +7,6 @@ export default function Carregando(props) {
                 !props.carregando ? props.children :
                     <div className="d-flex align-items-center m-5">
                         <strong role="status">Carregando...</strong>
-                        <Spinner animation="border" size="sm" variant="primary" />
                         <Spinner animation="border" variant="primary" />
 
                     </div>
